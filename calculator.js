ed7fec05db0a24aa089479f0509cb5fbcbc007f2
Timmy Kwesi <85815959+TimmyKwesi@users.noreply.github.com>
2022-06-26 05:37:37 +0000

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,26 @@
 // Simple JavaScript Calculator
+// take the operator input
+const operator = prompt('Enter operator: (+, -, * or / ): ');
+
+
+const no1 = prompt('Enter first number: ');
+const no2 = prompt('Enter second number: ');
+
+let result;
+
+
+if (operator == '+') {
+    result = no1 + no2;
+}
+else if (operator == '-') {
+    result = no1 - no2;
+}
+else if (operator == '*') {
+    result = no1 * no2;
+}
+else {
+    result = no1 / no2;
+}
+
+
+console.log(result);
